cf6060b75d11712fa4857d32629cb92b152400be
GnaixEuy <SGGHCJ@live.com>
2023-08-04 23:50:51 +0800

    :tada: begin project - a backend project about fake rebook

---
+++ service/uaa-service/src/main/java/cn/gnaixeuy/uaaservice/UaaServiceApplication.java
@@ -0,0 +1,20 @@
+package cn.gnaixeuy.uaaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ *
+ * @author GnaixEuy
+ * @version 1.0
+ * @see <a href="https://github.com/GnaixEuy">GnaixEuy</a>
+ */
+@SpringBootApplication
+public class UaaServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UaaServiceApplication.class, args);
+    }
+
+}
---
+++ service/uaa-service/src/test/java/cn/gnaixeuy/uaaservice/UaaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.gnaixeuy.uaaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UaaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
