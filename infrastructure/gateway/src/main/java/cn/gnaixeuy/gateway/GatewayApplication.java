b8fc2e2f105f22e574306a9772bd6e389abde23b
GnaixEuy <SGGHCJ@live.com>
2023-08-05 02:54:22 +0800

    :beers: Adjust the pom structure and introduce the gateway

--- infrastructure/gateway/src/main/java/cn/gnaixeuy/gateway/GatewayApplication.java
+++ infrastructure/gateway/src/main/java/cn/gnaixeuy/gateway/GatewayApplication.java
@@ -1,10 +1,7 @@
-package cn.gnaixeuy.uaaservice;
+package cn.gnaixeuy.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * <img src="https://img1.baidu.com/it/u=2537966709,2852517020&fm=253&fmt=auto&app=138&f=JPEG?w=648&h=489"/>
@@ -14,15 +11,11 @@ import org.springframework.scheduling.annotation.EnableAsync;
  * @version 1.0
  * @see <a href="https://github.com/GnaixEuy">GnaixEuy</a>
  */
-
-@EnableAsync
-@EnableFeignClients
-@EnableDiscoveryClient
 @SpringBootApplication
-public class UaaServiceApplication {
+public class GatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UaaServiceApplication.class, args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
 
 }
