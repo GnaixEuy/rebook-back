4a4438fbb4f4059082bfb236db4298e610153fd7
GnaixEuy <SGGHCJ@live.com>
2023-08-06 23:54:07 +0800

    :bug: fix gateway forwarding exception

--- infrastructure/gateway/src/main/java/cn/gnaixeuy/gateway/GatewayApplication.java
+++ infrastructure/gateway/src/main/java/cn/gnaixeuy/gateway/GatewayApplication.java
@@ -2,6 +2,9 @@ package cn.gnaixeuy.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * <img src="https://img1.baidu.com/it/u=2537966709,2852517020&fm=253&fmt=auto&app=138&f=JPEG?w=648&h=489"/>
@@ -11,6 +14,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @version 1.0
  * @see <a href="https://github.com/GnaixEuy">GnaixEuy</a>
  */
+@EnableAsync
+@EnableFeignClients
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayApplication {
 
